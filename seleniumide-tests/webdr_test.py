902d700ac787ac1a35f6cfdd026e97502c46d55f
Tibor <leeltib@gmail.com>
2021-06-13 00:15:57 +0200

    kutatás...

--- seleniumide-tests/webdr_test.py
+++ seleniumide-tests/webdr_test.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+# "homokozó" letiltása...
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path="/root/chromedriver", chrome_options=options)
+
+# driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
