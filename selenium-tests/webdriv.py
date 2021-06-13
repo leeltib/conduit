38ba582560836400eee6d714abfb8b3dc755ce4f
Tibor <leeltib@gmail.com>
2021-06-13 11:35:14 +0200

    webdriver test

--- selenium-tests/webdriv.py
+++ selenium-tests/webdriv.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
+# driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
@@ -11,12 +12,4 @@ elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
