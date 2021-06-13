73aa51b44a49baa33ef4d80cd18ad74fbd91aba2
Tibor <leeltib@gmail.com>
2021-06-13 10:20:54 +0200

    selenium-test mappa és py teszt létrehozása

--- selenium-tests/webdriv.py
+++ selenium-tests/webdriv.py
@@ -1,15 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
-# "homokozó" letiltása...
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-# options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path="/root/chromedriver", chrome_options=options)
-
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
