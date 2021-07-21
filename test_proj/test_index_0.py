aa074c3549dbdb118dfc44fd791b366c1292d8a3
Tibor <leeltib@gmail.com>
2021-07-22 00:19:06 +0200

    test_project A001-A008

--- test_proj/test_index_0.py
+++ test_proj/test_index_0.py
@@ -9,7 +9,7 @@
 # tcA001_run = A001 test case -  Új felhasználó regisztrációja felhasználó név, email cím és jelszó megadásával.
 tcA001_run = True
 # tcA002_run = A002 test case - Belépés a regisztráció során megadott adatokkal - kilépés.
-tcA002_run = False
+tcA002_run = True
 # tcA003_run = A003 test case - Új blogbejegyzés készítése - kilépés.
 tcA003_run = False
 # tcA004_run = A004 test case - Saját, meglévő blogbejegyzés módosítása - kilépés.
